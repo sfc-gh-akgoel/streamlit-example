46b420ccfd051be4e2b58dc01be975ae22084f55
Akkaash Goel <136388733+sfc-gh-akgoel@users.noreply.github.com>
2023-09-20 23:35:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ Test Commit 1
 
 
 with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+    total_points = st.slider("Number of points in spiral [test]", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
     Point = namedtuple('Point', 'x y')
